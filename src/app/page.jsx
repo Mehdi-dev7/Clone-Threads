eb0423bbb5cdd5e40a6623d1e9fdd4af09657547
Wil-dev <mehdididou7@icloud.com>
2025-02-17 15:53:04 +0100

    création des composants Post & ConnectLayout

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,53 @@
+import ConnectedLayout from "@/components/ConectedLayout/ConnectedLayout";
+import Post from "@/components/Post/Post";
+
 export default function Index() {
-  return (
-    <div>Accueil</div>
-  )
+	const posts = [
+		{
+			_id: "1",
+			content: "Bienvenu sur mon tout nouveau profil Threads !",
+			pseudo: "johnDoe",
+			profile: "/picture.png",
+		},
+		{
+			_id: "2",
+			content: "Bienvenu sur mon tout nouveau profil Threads !",
+			pseudo: "johnDoe",
+			profile: "/picture.png",
+		},
+		{
+			_id: "3",
+			content: "Bienvenu sur mon tout nouveau profil Threads !",
+			pseudo: "johnDoe",
+			profile: "/picture.png",
+		},
+		{
+			_id: "4",
+			content: "Bienvenu sur mon tout nouveau profil Threads !",
+			pseudo: "johnDoe",
+			profile: "/picture.png",
+		},
+		{
+			_id: "5",
+			content: "Bienvenu sur mon tout nouveau profil Threads !",
+			pseudo: "johnDoe",
+			profile: "/picture.png",
+		},
+	];
+	return (
+		<ConnectedLayout>
+			<div className="md:w-[700px] w-full mx-auto mt-10">
+				{/* New post */}
+
+				{/* Posts */}
+				{posts.map((post) => (
+          
+          <div key={post._id}>
+						<Post post={post} />
+					</div>
+				))}
+			</div>
+		</ConnectedLayout>
+    
+	);
 }
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,9 @@
 "use client";
 
-export default function Button({ children, onClick }) {
+export default function Button({ children, onClick, withoutMarginTop }) {
 	return (
 		<button
-			className="bg-white rounded-3xl border-threads-gray-light w-full mt-4 p-4 hover:bg-gray-300 duration-150"
+			className={`bg-white rounded-3xl border-threads-gray-light w-full ${!withoutMarginTop && "mt-4"} p-4 hover:bg-gray-300 duration-150"`}
 			onClick={onClick}
 		>
 			{children}
